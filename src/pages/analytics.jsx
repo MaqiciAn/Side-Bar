eff4516fb22c34d8b640ff1be7cae8854b3f9a2b
melih <melhcicek@gmail.com>
2022-07-23 15:06:48 +0300

    Side-bar kodlar
    Temel site kodları

---
+++ src/pages/analytics.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const analytics =() =>{
+    return(
+        <div style={{borderBottom:'1px solid black'}}>
+            <h1>Analytics</h1>
+        </div>
+    );
+};
+
+export default analytics;
---
+++ src/pages/content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const content =() =>{
+    return(
+        <div style={{borderBottom:'1px solid black'}}>
+            <h1>Content Manager</h1>
+        </div>
+    );
+};
+
+export default content;
